8704af4f98a77e7de584e03a5ef2b892bde47f04
Eran Raskansky <eran@rapidapi.com>
2020-02-02 13:35:54 +0200

    wip

--- src/renderer/redux/reducers/apiGatewayProd.reducer.js
+++ src/renderer/redux/reducers/apiGatewayProd.reducer.js
@@ -1,4 +1,4 @@
-import { SET_API_GATEWAY_PROD_RESPONSE, NOTIFY_MP_PROD_CHANGES, SET_INIT_PROD_API_GATEWAY_DATA } from '../actionTypes';
+import { SET_API_GATEWAY_PROD_RESPONSE, NOTIFY_API_GATEWAY_PROD_CHANGES, SET_INIT_PROD_API_GATEWAY_DATA } from '../actionTypes';
 
 const initialState = {
   status: null,
@@ -14,7 +14,7 @@ export default (state = initialState, action) => {
       return { ...state, pingHistory: action.payload };
     case SET_API_GATEWAY_PROD_RESPONSE:
       return { ...state, ...action.payload, pingHistory: [...state.pingHistory, action.payload.status] };
-    case NOTIFY_MP_PROD_CHANGES:
+    case NOTIFY_API_GATEWAY_PROD_CHANGES:
       return { ...state, notifyChanges: action.payload };
     default:
       return state;
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export const displayThis = (condition, display = 'block') => (condition ? display : 'none');
-export const displayStyle = (condition, display) => ({ display: displayThis(condition, display) });
