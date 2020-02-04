60829707c9f880cc5fae2d2af382c0064ee83d60
Eran Raskansky <eran@rapidapi.com>
2020-02-04 21:47:15 +0200

    feat: adds plop for redux

--- src/renderer/redux/epics/index.js
+++ src/renderer/redux/epics/index.js
@@ -1,8 +1,17 @@
 import { combineEpics } from 'redux-observable';
+
+// PLOP IMPORT EPICS PLACEHOLDER
 import mpProdEpic from './mpProd.epic';
 import apiGatewayProd from './apiGatewayProd.epic';
 import aclProd from './aclProd.epic';
 import apiProd from './apiProd.epic';
 import orgDashboardProd from './orgDashboardProd.epic';
 
-export default combineEpics(...mpProdEpic, ...apiGatewayProd, ...aclProd, ...orgDashboardProd, ...apiProd);
+export default combineEpics(
+  // PLOP COMBINE_EPICS PLACEHOLDER
+  ...mpProdEpic,
+  ...apiGatewayProd,
+  ...aclProd,
+  ...orgDashboardProd,
+  ...apiProd,
+);
